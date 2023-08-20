fb08a8b5a441252a199bc2bc83aa82129166ea9c
Hector <HC-d4!@MBP-de-HC.local>
2023-08-20 14:37:41 -0400

    adding  base routers

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
-const router = Router();
+const playerRoutes = Router();
 
-router.get('/:player', (req, res) => {
+playerRoutes.get('/:player', (req, res) => {
     const player = req.params.player;
     res.send(`Obteniendo informacion de ${player}`);
 });
 
-router.get('/:player/last', (req, res) => {
+playerRoutes.get('/:player/last', (req, res) => {
     const player = req.params.player;
     res.send(`Obteniendo ultima partida de ${player}`);
 });
 
-export default router;
+export default playerRoutes;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 3000;
 app.use(json());
 
 // Importar rutas
-import routes from './controllers/gameController.js';
+import gameRoutes from './controllers/gameController.js';
+import playerRoutes from './controllers/playerController.js';
 
 // Usar las rutas
-app.use('/chessflix/api/games', routes);
+app.use('/chessflix/api/games', gameRoutes);
+app.use('/chessflix/api/player', playerRoutes);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
